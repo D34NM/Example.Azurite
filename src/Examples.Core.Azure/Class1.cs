cbee0ba8dec1f6b44f5192d5fb591f6b1398d52b
Dean Milojevic <milojevicdean@gmail.com>
2020-01-14 11:46:55 +0100

    Initial structure of the project

---
+++ src/Examples.Core.Azure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Examples.Core.Azure
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Examples.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Examples.Core
+{
+    public class Class1
+    {
+    }
+}
